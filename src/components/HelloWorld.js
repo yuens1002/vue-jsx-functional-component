14300f2e57c08ce98a5ba9999285e43ab274282d
Sunny Yuen <syuen@metrostarsystems.com>
2021-12-31 08:06:32 -0800

    give functional component a name in vue dev tools

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,19 +1,20 @@
 import Wrapper from './Wrapper';
 
-export default ({ props }) => {
+const re = /<a\b[^>]*>(.*?)<\/a>/gi;
+const rePun = /[!?.,:;]/gi;
+const links = [];
+const pattern = '<replace>';
+let matchIdx = 0;
+
+const HelloWorld = ({ props }) => {
   function createStrComp(msg) {
-    const re = /<a\b[^>]*>(.*?)<\/a>/gi;
-    const rePun = /[!?.,:;]/gi;
-    const links = [];
-    const pattern = '<replace>';
-    let matchIdx = 0;
-    const s1 = msg.replace(rePun, (match) => ' ' + match);
-    const s2 = s1.replace(re, (_, p1) => {
+    const s1 = msg.replace(re, (_, p1) => {
       // account for each link
       links.push(p1);
       // add the pattern to the string
       return pattern;
     });
+    const s2 = s1.replace(rePun, (match) => ' ' + match);
     const arrStr = s2.split(' ');
     const arrStrComp = arrStr.map((str) => {
       // replace the pattern with the componenet
@@ -22,7 +23,6 @@ export default ({ props }) => {
       }
       return str;
     });
-
     return arrStrComp.reduce((all, item, i) => {
       // add a space before all non punctuated words
       if (typeof item === 'string' && item.search(rePun) !== 0) {
@@ -30,9 +30,10 @@ export default ({ props }) => {
       }
       // add a space before a link
       if (typeof item !== 'string' && i !== 0) all.push(' ');
-      all.push(item);
-      return all;
+      return all.push(item), all;
     }, []);
   }
   return <div>{createStrComp(props.msg)}</div>;
 };
+
+export default HelloWorld;
